729befd64c917547242de029af39e69ccba7a569
millooo94 <camillomusmeciunict@gmail.com>
2022-11-09 17:28:14 +0100

    refactor all

--- js/script.js
+++ js/script.js
@@ -10,112 +10,112 @@ const arrIcons = [
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'crow',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'dog',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'dove',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'dragon',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'horse',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'hippo',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'fish',
 		prefix: 'fa-',
 		type: 'animal',
 		family: 'fas',
-		color: randomColor
+		color: 'orange'
 	},
 	{
 		name: 'carrot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: randomColor
+		color: 'green'
 	},
 	{
 		name: 'apple-alt',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: randomColor
+		color: 'green'
 	},
 	{
 		name: 'lemon',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: randomColor
+		color: 'green'
 	},
 	{
 		name: 'pepper-hot',
 		prefix: 'fa-',
 		type: 'vegetable',
 		family: 'fas',
-		color: randomColor
+		color: 'green'
 	},
 	{
 		name: 'user-astronaut',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: randomColor
+		color: 'blue'
 	},
 	{
 		name: 'user-graduate',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: randomColor
+		color: 'blue'
 	},
 	{
 		name: 'user-ninja',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: randomColor
+		color: 'blue'
 	},
 	{
 		name: 'user-secret',
 		prefix: 'fa-',
 		type: 'user',
 		family: 'fas',
-		color: randomColor
+		color: 'blue'
 	}
 ];
 
@@ -158,8 +158,7 @@ arrIcons.forEach(element => {
     iconContainer.append(eleIcon)
     eleIcon.classList.add(`${element.family}`)
     eleIcon.classList.add(`${element.prefix}${element.name}`)
-    eleIcon.style.color = `#${element.color}`
-   
+    eleIcon.style.color = `${element.color}`
 });
 
 
@@ -181,7 +180,7 @@ eleSelect.addEventListener('click', function(){
             iconContainer.append(eleIcon)
             eleIcon.classList.add(`${element.family}`)
             eleIcon.classList.add(`${element.prefix}${element.name}`)
-            eleIcon.style.color = `#${element.color}`
+            eleIcon.style.color = `${element.color}`
            
         });
         
@@ -200,7 +199,7 @@ eleSelect.addEventListener('click', function(){
             iconContainer.append(eleIcon)
             eleIcon.classList.add(`${element.family}`)
             eleIcon.classList.add(`${element.prefix}${element.name}`)
-            eleIcon.style.color = `#${element.color}`
+            eleIcon.style.color = `${element.color}`
            
         });
 
@@ -221,7 +220,7 @@ eleSelect.addEventListener('click', function(){
             iconContainer.append(eleIcon)
             eleIcon.classList.add(`${element.family}`)
             eleIcon.classList.add(`${element.prefix}${element.name}`)
-            eleIcon.style.color = `#${element.color}`
+            eleIcon.style.color = `${element.color}`
            
         });
 
@@ -243,8 +242,7 @@ eleSelect.addEventListener('click', function(){
             iconContainer.append(eleIcon)
             eleIcon.classList.add(`${element.family}`)
             eleIcon.classList.add(`${element.prefix}${element.name}`)
-            eleIcon.style.color = `#${element.color}`
-         
+            eleIcon.style.color = `${element.color}`
         });
 
     }
